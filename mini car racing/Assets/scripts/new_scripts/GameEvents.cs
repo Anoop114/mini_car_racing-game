9e7c6b9a79e4d259f5dd72462ff6ba0975c2d5b6
Anoop Kumar Sharma <69163049+Anoop114@users.noreply.github.com>
2021-07-10 22:27:59 +0530

    Event System Complete
    Completed The Event System as the car move without using update function on its script.

--- mini car racing/Assets/scripts/new_scripts/GameEvents.cs
+++ mini car racing/Assets/scripts/new_scripts/GameEvents.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 
 public class GameEvents : MonoBehaviour
 {
-    public event Action<float,float,float> onMoveCar;
+    public static GameEvents instance;
+    public event Action<float,float,float> OnMoveCar;
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(this);
+        }
+    }
+    // public void MoveCar()
+    // {
+    //     float a = Input.GetAxis("Vertical");
+    //     float s = Input.GetAxis("Horizontal");
+    //     float b = Input.GetAxis("Jump");
+    //     OnMoveCar?.Invoke(a,s,b);
+    // }
     void Update()
     {
         float a = Input.GetAxis("Vertical");
         float s = Input.GetAxis("Horizontal");
         float b = Input.GetAxis("Jump");
-        if(onMoveCar != null)
-        {
-            onMoveCar(a,s,b);
-        }
+        OnMoveCar?.Invoke(a,s,b);
     }
 }
--- mini car racing/Assets/scripts/new_scripts/car_move.cs
+++ mini car racing/Assets/scripts/new_scripts/car_move.cs
@@ -1,27 +1,24 @@
 ﻿using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class car_move : MonoBehaviour
+public class Car_move : MonoBehaviour
 {
+    // [SerializeField] UnityEvent OnCompleteEvent;
     public WheelCollider[] Wcs;
     public GameObject[] Wheels;
     public float Torque = 200f;
     public float BrakeTorque = 500f;
     public float MaxSteerAngle = 30f;
 
-    
-    void Start()
+    void OnEnable()
     {
-        GameEvents CarMove = GetComponent<GameEvents>();
-        CarMove.onMoveCar += test;
+        GameEvents.instance.OnMoveCar += Go;    
     }
-    private void test(float a,float b,float c)
-    {
-        Debug.Log(a+" "+b+" "+c);
-    }
-    private void Go(float acc, float steer, float brake)
+    public void Go(float acc,float steer,float brake)
     {
         acc = Mathf.Clamp(acc,-1,1)*Torque;
         steer = Mathf.Clamp(steer,-1,1)*MaxSteerAngle;
@@ -43,14 +40,5 @@ public class car_move : MonoBehaviour
             Wheels[i].transform.position = pos;
             Wheels[i].transform.rotation = quat;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float a = Input.GetAxis("Vertical");
-        float s = Input.GetAxis("Horizontal");
-        float b = Input.GetAxis("Jump");
-        Go(a,s,b);
     }
 }
